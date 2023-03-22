4e26b490efdbc47c03bcf8b0aaf09985e0a28e92
Prasanna Dubey <47008212+luciferpd13@users.noreply.github.com>
2023-03-22 16:33:34 +0530

    Pushing my first endpoint on golang

--- src/index.go
+++ src/index.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-   "io/ioutil"
+   "fmt"
    "log"
    "net/http"
 )
 
+func handleReq(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/hello" {
+		http.Error(res, "404 not found.", http.StatusNotFound)
+	}
+
+	if req.Method != "GET" {
+		http.Error(res, "Method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(res, "This is working")
+}
+
+
 func main() {
-   resp, err := http.Get("https://luciferpd13.github.io/sales.json")
-   if err != nil {
-      log.Fatalln(err)
-   }
-//We Read the response body on the line below.
-   body, err := ioutil.ReadAll(resp.Body)
-   if err != nil {
-      log.Fatalln(err)
-   }
-//Convert the body to type string
-   sb := string(body)
-   log.Printf(sb)
+	const port string = ":3000";
+
+	http.HandleFunc("/hello", handleReq)
+
+	fmt.Printf("Server starting at port no." + port)
+	if err:=http.ListenAndServe(port, nil); err != nil{
+		log.Fatal(err);
+	}
 }
